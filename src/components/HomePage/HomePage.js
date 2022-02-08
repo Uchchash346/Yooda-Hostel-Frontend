228ea588915b3d6b7a45c3c3020d510fe198a608
Uchchash346 <mru416@gmail.com>
2022-02-08 11:44:33 +0600

    Home Page Setup

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     return (
         <div>
             <div className="col-md-1 z1 d-flex align-items-center justify-content-center container pt-5 lottie-icon">
-                <lottie-player className="" src="https://assets9.lottiefiles.com/temp/lf20_nXwOJj.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                <lottie-player className="" src="https://assets9.lottiefiles.com/packages/lf20_vIqsDp.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
             <div className="container d-flex align-items-center justify-content-center button-group-area pt-3">
                 <button type="button" class="btn btn-outline-success btn-lg ms-3 text-bold">Food Item</button>
