c338bb27bc5aa8017bda1c3813fffc66267b970f
Uchchash346 <mru416@gmail.com>
2022-02-10 21:42:43 +0600

    updated food item functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import AddFoodItem from './components/FoodItem/AddFoodItem';
 import FoodItem from './components/FoodItem/FoodItem';
 import UpdateFoodItem from './components/FoodItem/UpdateFoodItem';
-import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import DisplayStudentInfo from './components/StudentInfo/DisplayStudentInfo';
 import StudentInfo from './components/StudentInfo/StudentInfo';
@@ -12,7 +11,6 @@ import UpdateStudentInfo from './components/StudentInfo/UpdateStudentInfo';
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/foodItem" element={<FoodItem />} />
